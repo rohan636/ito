b9e698027c3cd78655f19209a7b9748d13cd08b8
Eddi <eddi1337@gmail.com>
2015-02-05 18:10:35 +0000

    Updated Homepage
    -Added descriptions
    -Updated main picture

--- index.php
+++ index.php
@@ -24,10 +24,11 @@
                 <p>Jay-Bee Telecom represents BT Local Business in the Herts & Essex areas. </p>
                 <p>For more information on BT Local Business and our products click below</p>
                 <p>
-                    <a class="btn btn-default btn-lg" href="www.bt.com/btlocalbusiness">BT Local Business &raquo;</a>
+                    <a class="btn btn-default btn-lg" href="http://www.bt.com/btlocalbusiness">BT Local Business >> </a>
                 </p>
             </div>
             <div class="col-sm-4">
+                <!-- Repalce with widget
                 <h2>Contact Us</h2>
                 <address>
                     <strong>Location</strong>
@@ -40,31 +41,37 @@
                     <br>
                     <abbr title="Email">E:</abbr> <a href="mailto:#">name@example.com</a>
                 </address>
+                -->
             </div>
+            
         </div>
         <!-- /.row -->
-            <!-- src="http://placehold.it/300x300" -->
+            
         <hr>
         <div class="row">
             <div class="col-sm-4">
                 <img class="img-circle img-responsive img-center" src="img/Plans.jpg" alt="">
                 <h2>Current Plans</h2>
-                <p>Download speed upto: 17mbps <br/>
-                Upload speed upto: 1 mbps <br/>
-                How much data can I use every month? = 10GB <br/>
-                Price: £10 per month(for 24 months) or £14 per month(for 12 months)
+                <p>Find out information about the new plans we have to offer, as well as alternative upgrade plans. 
                 </p>
             </div>
             <div class="col-sm-4">
                 <img class="img-circle img-responsive img-center" src="img/Upgrade.jpg" alt="">
-                <h2>Why upgrade to Infinity</h2>
-                <p>Go for <b>BT Infinity</b> superfast fibre optic broadband. <br/>
-                & Find out why BT is the most popular broadband provider in the UK</p>
+                <h2>Upgrade to Infinity</h2>
+                <p>Go for <b>BT Infinity</b> superfast fibre optic broadband. 
+                <br/>& Find out why BT is the most popular broadband provider in the UK.
+                <br/>Also view the five other reasons to switch to BT</p>
+                <br/><a class="btn btn-default btn-lg" href="#">Upgrade</a>
             </div>
             <div class="col-sm-4">
                 <img class="img-circle img-responsive img-center" src="img/Customer.jpg" alt="">
                 <h2>Customer Service</h2>
-                <p>we respond to customers within 24 hours, and have consultents based in the UK</p>
+                <p>We respond to any enquires within 24 hours, we also and have consultents based in the UK. 
+                <br/>Find out how to contact us on our contact page, or you can click below to get there.
+                <br/>Also the BT Business Technical Team is there to support you, 24/7 for any technical issues.</p>
+                <br/><a class="btn btn-default btn-lg" href="#">Contact us</a>
+
+                <!-- Add customer link -->
             </div>
 
         </div>
