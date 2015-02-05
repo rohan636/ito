e568eb36208cc0804400e376dfe189e351b4bd5f
Eddi <eddi1337@gmail.com>
2015-02-05 17:23:47 +0000

    Update Homepage
    -Added description
    -Added Images

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="tagline">Business Name or Tagline</h1>
+                    <h1 class="tagline">Jay-Bee Telecom</h1>
                 </div>
             </div>
         </div>
@@ -20,47 +20,53 @@
         <div class="row">
             <div class="col-sm-8">
                 <h2>What We Do</h2>
-                <p>Introduce the visitor to the business using clear, informative text. Use well-targeted keywords within your sentences to make sure search engines can find the business.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et molestiae similique eligendi reiciendis sunt distinctio odit? Quia, neque, ipsa, adipisci quisquam ullam deserunt accusantium illo iste exercitationem nemo voluptates asperiores.</p>
+                <p>BT Local Business set up over 10 years ago provides small and medium enterprises with BT's full range of communications products and services included calls, lines, broadband, Internet services, mobile phone services and phone systems.</p>
+                <p>Jay-Bee Telecom represents BT Local Business in the Herts & Essex areas. </p>
+                <p>For more information on BT Local Business and our products click below</p>
                 <p>
-                    <a class="btn btn-default btn-lg" href="#">Call to Action &raquo;</a>
+                    <a class="btn btn-default btn-lg" href="www.bt.com/btlocalbusiness">BT Local Business &raquo;</a>
                 </p>
             </div>
             <div class="col-sm-4">
                 <h2>Contact Us</h2>
                 <address>
-                    <strong>Start Bootstrap</strong>
-                    <br>3481 Melrose Place
-                    <br>Beverly Hills, CA 90210
+                    <strong>Location</strong>
+                    <br>Cambridge House, Cambridge Rd,
+                    <br>Harlow, Essex, CM20 2EQ
                     <br>
                 </address>
                 <address>
-                    <abbr title="Phone">P:</abbr>(123) 456-7890
+                    <abbr title="Phone">P:</abbr>(+44) 01279 634666
                     <br>
                     <abbr title="Email">E:</abbr> <a href="mailto:#">name@example.com</a>
                 </address>
             </div>
         </div>
         <!-- /.row -->
-
+            <!-- src="http://placehold.it/300x300" -->
         <hr>
-
         <div class="row">
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
-                <h2>Marketing Box #1</h2>
-                <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
+                <img class="img-circle img-responsive img-center" src="img/Plans.jpg" alt="">
+                <h2>Current Plans</h2>
+                <p>Download speed upto: 17mbps <br/>
+                Upload speed upto: 1 mbps <br/>
+                How much data can I use every month? = 10GB <br/>
+                Price: £10 per month(for 24 months) or £14 per month(for 12 months)
+                </p>
             </div>
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
-                <h2>Marketing Box #2</h2>
-                <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <img class="img-circle img-responsive img-center" src="img/Upgrade.jpg" alt="">
+                <h2>Why upgrade to Infinity</h2>
+                <p>Go for <b>BT Infinity</b> superfast fibre optic broadband. <br/>
+                & Find out why BT is the most popular broadband provider in the UK</p>
             </div>
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
-                <h2>Marketing Box #3</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                <img class="img-circle img-responsive img-center" src="img/Customer.jpg" alt="">
+                <h2>Customer Service</h2>
+                <p>we respond to customers within 24 hours, and have consultents based in the UK</p>
             </div>
+
         </div>
         <!-- /.row -->
 
