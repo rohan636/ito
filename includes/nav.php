b86a9e7ed9294b492fc0e2ed2492e6aaec34a2ad
rohan636 <rohan@spanad.com>
2015-02-05 15:44:46 +0000

    Changes made to index file

---
+++ includes/nav.php
@@ -0,0 +1,55 @@
+<?php include ('head.php');?>
+
+<body>
+
+    <!-- Navigation -->
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">
+        <img alt="logo" src="img/logo.png" height="32px">
+      </a>
+    </div>
+        <center>
+            <div class="navbar-collapse collapse" id="navbar-main">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a>
+                    </li>
+                    <li><a href="#">BT.com</a>
+                    </li>
+                    <li><a href="#">Plans</a>
+                    </li>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">More BT Sites<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">BT WIFI</a>
+                            </li>
+                            <li><a href="#">BT Cloud</a>
+                            </li>
+                            <li><a href="#">BT Shop</a>
+                            </li>
+                            <li class="divider"></li>
+                            <li><a href="#">BT Phone Book</a>
+                            </li>
+                            <li class="divider"></li>
+                            <li><a href="#">BT Redcare</a>
+                            </li>
+                            </ul>
+                   			 <li><a href="#">Contact Us</a>
+                   			 </li>
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="password" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-default">Sign In</button>
+                </form>
+            </div>
+        </center>
+    </div>
+</div>
